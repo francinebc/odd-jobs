031a4d0a4106f6855c2865113f4c1f701a36e800
francinebc <francinebc.mail@gmail.com>
2019-04-02 13:11:15 +1300

    Can now login but page not refreshing

--- src/app/client/api/auth.ts
+++ src/app/client/api/auth.ts
@@ -1,11 +1,18 @@
 import * as request from "superagent"
-import { SignUpUser } from '../utils/types'
+import { SignUpUser, LoginUser } from '../utils/types'
 
 const url = 'http://localhost:3000/api/v1/auth'
 
 export function signUp (user: SignUpUser) {
   return request
     .post(`${url}/signUp`)
+    .send(user)
+    .then(res => res.body)
+}
+
+export function login (user: LoginUser) {
+  return request
+    .post(`${url}/login`)
     .send(user)
     .then(res => res.body)
 }
