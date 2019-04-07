ffa935422f669e0a71ea849b05998b604711b281
francinebc <francinebc.mail@gmail.com>
2019-04-08 10:42:48 +1200

    added tests but they cannot fail

--- src/tests/server/db/users.test.js
+++ src/tests/server/db/users.test.js
@@ -17,6 +17,18 @@ beforeEach(() => {
 
 afterEach(() => testEnv.cleanup(testDb))
 
+test('insertUser returns id', () => {
+  const expectedId = 3
+  const hash = "akbsafbf"
+  return db
+    .insertUser(user, hash, testDb)
+    .then((data) => {
+      const actualId = data[0]
+      expect(actualId).toBe(expectedId)
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
 test('signUpUser registers a new user', () => {
   const expectedId = 3
 
@@ -44,14 +56,46 @@ test('signUpUser does not re-signup same user', () => {
     .catch(err => expect(err).toBeNull())
 })
 
-test('insertUser returns id', () => {
-  const expectedId = 3
-  const hash = "akbsafbf"
+// cannot make this test fail
+test('signUpUser hashes password', () => {
   return db
-    .insertUser(user, hash, testDb)
-    .then((data) => {
-      const actualId = data[0]
-      expect(actualId).toBe(expectedId)
+    .signUpUser(user, testDb)
+    .then(() => {
+      db.getUser(user, testDb)
+        .then(data => {
+          const matched = user.password === data.hash
+          expect(matched).toBeFalsy()
+        })
+        .catch(err => expect(err).toBeNull())
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+// cannot make this test fail
+test('getUser returns user with correct email', () => {
+  return db
+    .signUpUser(user, testDb)
+    .then(() => {
+      db.getUser(user, testDb)
+        .then(data => {
+          expect(user.email).toMatch(data.email)
+        })
+        .catch(err => expect(err).toBeNull())
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+//cannot make this test fail
+test('getUser returns undefined with incorrect email', () => {
+  return db
+    .signUpUser(user, testDb)
+    .then(() => {
+      user.email = 'sammy'
+      db.getUser(user, testDb)
+        .then(data => {
+          expect(data).toBeUndefined()
+        })
+        .catch(err => expect(err).toBeNull())
     })
     .catch(err => expect(err).toBeNull())
 })
